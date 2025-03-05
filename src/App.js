6296086d64b93b07b33cdcf4644082848b771f20
Kausheya2006 <royrimo2006@gmail.com>
2025-03-06 00:18:45 +0530

    sample commit message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <p>This is a simple React0 frontend running inside a Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
